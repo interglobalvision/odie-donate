e458471181625f23fda65b05ca9b52194ae0f8f8
mikeyrayvon <mikey@interglobal.vision>
2018-06-15 15:48:03 -0500

    add error conditions

--- twitter.php
+++ twitter.php
@@ -3,6 +3,15 @@ require_once('vendor/autoload.php');
 require_once('secret-keys.php');
 
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
-$statuses = $twitter->load(Twitter::ME);
-echo json_encode($statuses);
+
+if (!$twitter->authenticate()) {
+	die('Invalid name or password');
+}
+
+try {
+	$statuses = $twitter->load(Twitter::ME);
+  echo json_encode($statuses);
+} catch (TwitterException $e) {
+	echo "Error: ", $e->getMessage();
+}
 ?>
