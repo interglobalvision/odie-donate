423d279ad701638ab8b15ac1e9789ae2aa22d9d4
mikeyrayvon <mikey@interglobal.vision>
2018-06-15 15:45:50 -0500

    json_encode

--- twitter.php
+++ twitter.php
@@ -4,5 +4,5 @@ require_once('secret-keys.php');
 
 $twitter = new Twitter($consumerKey, $consumerSecret, $accessToken, $accessTokenSecret);
 $statuses = $twitter->load(Twitter::ME);
-echo $statuses;
+echo json_encode($statuses);
 ?>
